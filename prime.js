8a7ee2ac72de39b67a8e59e98fdf304ad0b0c772
Ankit Arora <ankitaroraarora11@gmail.com>
2019-06-23 19:24:30 +0530

    Prog to no. check prime or not

---
+++ prime.js
@@ -0,0 +1,21 @@
+function prime(n)
+{
+	if(n===1)
+{
+	return false;
+}
+else
+{
+	for(var i=2; i<n ; i++)
+	{
+		if(num % i===0)
+		{
+			window.alert("not prime number")
+		}
+}
+window.alert("this is prime number");
+}
+}
+console.log(prime(2));      
+
+
