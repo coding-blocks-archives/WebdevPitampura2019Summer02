1696aa37ae2341a1558850fd4d1e9cb81f16269c
imdhruvgupta <gdhruv167@gmail.com>
2019-06-24 10:01:55 +0530

    Fizzbuzz

--- Lecture06/Fizzbuzz/script.js
+++ Lecture06/Fizzbuzz/script.js
@@ -4,16 +4,18 @@ let list = document.getElementById('list')
 
 function doFunction() {
     let start = new Date().getTime()
-    
-    let num = inputBox.value;
 
+    let num = inputBox.value;
+    let str = "";
     for(var i=1; i<=num; i++)
     {
-        if(i%3 == 0 && i%5==0) list.innerHTML += `<li>FizzBuzz</li>`;
-        else if(i%3==0) list.innerHTML += `<li>Fizz</li>`
-        else if(i%5==0) list.innerHTML += `<li>Buzz</li>`
-        else list.innerHTML += `<li>${i}</li>`
+        if(i%3 == 0 && i%5==0) str += `<li>FizzBuzz</li>`;
+        else if(i%3==0) str += `<li>Fizz</li>`
+        else if(i%5==0) str += `<li>Buzz</li>`
+        else str += `<li>${i}</li>`
     }
+
+    list.innerHTML +=str
 
     let end = new Date().getTime()
     console.log(end-start);
