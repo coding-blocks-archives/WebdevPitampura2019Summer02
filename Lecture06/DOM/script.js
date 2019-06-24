a7c91eef083deb8b115738b1a375be7616ac7752
imdhruvgupta <gdhruv167@gmail.com>
2019-06-24 08:53:06 +0530

    DOM Manipulation

---
+++ Lecture06/DOM/script.js
@@ -0,0 +1,8 @@
+let para = document.getElementById('para');
+let link = document.getElementById('link');
+
+para.innerText += " Dhruv";
+
+link.innerText = "Welcome to Coding Blocks"
+link.setAttribute('href', 'https://codingblocks.com/')
+
---
+++ Lecture06/Hoisting/script.js
@@ -0,0 +1,9 @@
+sayHello("Dhruv");
+let n = 5;
+
+function sayHello(name) {
+
+    for(var i=0; i<n; i++) {
+        console.log("Hello " + name)
+    }
+}
