4408d2d01017f53e53532037170b6cf9384aa81f
sumit saurabh <sumitsaurabh9868@gmail.com>
2019-06-27 23:00:13 +0530

    Make an array of github_id{Strings} from this array of objects

---
+++ gihub id array/github_id_array.js
@@ -0,0 +1,15 @@
+let oarray=[{
+    name:'Divyansh',
+    github_id:'silent-lad'
+  },{
+    name:'Abhishek',
+    github_id:'abhishek97'
+  },{
+    name:'Dhruv',
+    github_id:'imdhruvgupta'
+  }]
+
+  let narray=oarray.map((item)=>item.github_id)
+
+  console.log(narray);
+  
